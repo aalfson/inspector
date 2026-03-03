ce31d29232852e8f6e19ef90a5d9b05c67f4df0f
Aaron Alfson <aaron.alfson@gmail.com>
2026-03-03 12:42:46 -0500

    added recon

--- mix.exs
+++ mix.exs
@@ -21,8 +21,7 @@ defmodule Inspector.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:recon, "~> 2.5.6"}
     ]
   end
 end
